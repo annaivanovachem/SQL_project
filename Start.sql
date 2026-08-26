fb05ae712081fde76d0c54906b7085da4788d50f
annaivanovachem <anna.ivanova.chem@gmail.com>
2026-08-26 15:44:11 +0300

    Update Start.sql

--- Start.sql
+++ Start.sql
@@ -91,6 +91,15 @@ CREATE TABLE st.product_reviews (
         UNIQUE (customer_id, product_id)  
     );-- один клиент может оставить только один отзыв на товар   
 
+--Создание индексов на внешние ключи:
+CREATE INDEX idx_orders_customer_id ON st.orders(customer_id);  
+CREATE INDEX idx_order_items_order_id ON st.order_items(order_id);  
+CREATE INDEX idx_order_items_product_id ON st.order_items(product_id);  
+CREATE INDEX idx_status_history_order_id ON st.order_status_history(order_id);  
+CREATE INDEX idx_reviews_customer_id ON st.product_reviews(customer_id);  
+CREATE INDEX idx_reviews_product_id ON st.product_reviews(product_id);  
+CREATE INDEX idx_reviews_order_id ON st.product_reviews(order_id);  
+
 -- Вставка данных в таблицы. Все данные сгенерированы ИИ.  
 
 INSERT INTO st.customers (email, full_name, phone, address, city, registered_at, is_active) VALUES
